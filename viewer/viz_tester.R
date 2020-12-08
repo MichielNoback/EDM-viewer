9e48a86d68bf3a43b0e226159c908f982530c7a4
RonaldWedema <r.wedema@pl.hanze.nl>
2020-12-08 13:56:11 +0100

    add plotter testing module

---
+++ viewer/viz_tester.R
@@ -0,0 +1,9 @@
+source('../utils/visualization_functions.R')
+
+plot_spectral_bait_condition_using_uniprot('P36415', 'dicty')
+plot_spectral_bait_condition_using_uniprot('GST', 'dicty')
+plot_spectral_bait_condition_using_uniprot('GST', 'neutro')
+plot_spectral_bait_condition_using_uniprot('Q54YT4', 'dicty')
+plot_spectral_using_condition("Normal", 'dicty')
+plot_spectral_using_bait("Galpha8", 'dicty')
+boxplot_spectral_using_bait("Galpha8", 'dicty')
