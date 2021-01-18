dc20020f0159aa53302dd7233c540ae4af17fad8
ronald <wedema@home.nl>
2021-01-18 14:46:11 +0100

    test code reflects new function name and arguments

--- viewer/viz_tester.R
+++ viewer/viz_tester.R
@@ -1,18 +1,32 @@
 source('../utils/visualization_functions.R')
 
-plot_spectral_bait_condition_using_uniprot('P36415', 'dicty')
-plot_spectral_bait_condition_using_uniprot('GST', 'dicty')
-plot_spectral_bait_condition_using_uniprot('GST', 'neutro')
-plot_spectral_bait_condition_using_uniprot('Q54YT4', 'dicty')
-plot_spectral_using_condition("Normal", 'dicty')
-plot_spectral_using_bait("Galpha8", 'dicty')
 
-condition_boxplot_spectral_using_bait("Galpha8", 'dicty')
-experiment_boxplot_spectral_using_bait("Galpha8", 'dicty')
+# test uniprot + non normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', uniprot_id = 'GST', normalized = FALSE)
+# test uniprot + normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', uniprot_id = 'GST', normalized = TRUE)
 
+# test condition + normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', condition = 'Normal', normalized = TRUE)
 
-plot_normalized_spectral_bait_condition_using_uniprot('P36415', 'dicty')
-plot_normalized_spectral_bait_condition_using_uniprot('RASC', 'dicty')
-plot_normalized_spectral_bait_condition_using_uniprot('', 'dicty')
-condition_boxplot_normalized_spectral_using_bait("Galpha8", 'dicty')
-experiment_boxplot_normalized_spectral_using_bait("Galpha8", 'dicty')
+# test condition + non normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', condition = 'Normal', normalized = FALSE)
+
+# test bait + normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', bait_in = 'Galpha8', normalized = TRUE)
+
+# test bait + non normal
+vis_testing <- filter_data_and_plot(celltype = 'dicty', bait_in = 'Galpha8', normalized = FALSE)
+
+# test condition + boxplot  
+vis_testing <- filter_data_and_plot(celltype = 'dicty', bait_in = 'Galpha8', normalized = FALSE, viz_type = 'box')
+
+# test condition + boxplot + log scaling
+vis_testing <- filter_data_and_plot(celltype = 'dicty', bait = 'Galpha8', normalized = FALSE, viz_type = 'box', log_scale = 10)
+
+# test condition + boxplot + log scaling + normalized
+vis_testing <- filter_data_and_plot(celltype = 'dicty', bait = 'Galpha8', normalized = TRUE, viz_type = 'box', log_scale = 10)
+
+# test condition + boxplot + log scaling + normalized
+vis_testing <- filter_data_and_plot(celltype = 'dicty', experiment = TRUE, bait = 'Galpha8', normalized = FALSE, viz_type = 'box', log_scale = 10)
+
