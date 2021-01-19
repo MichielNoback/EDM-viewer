1b38e9e36fddc5b86bb1d22623bb6c7c7360c134
ronald <wedema@home.nl>
2021-01-19 11:37:23 +0100

    add filter function

--- data_mining/data_processing.R
+++ data_mining/data_processing.R
@@ -75,7 +75,7 @@ get_data <- function(what){
   all_data <- all_data %>% 
     mutate(across(c(bait, condition), as.factor))
     
-    # Z-score normalisation, by group and total, background should be excluded?
+    # Z-score normalisation, by group and total
     all_data <- all_data %>% 
       group_by(bait, condition) %>% 
       mutate(grouped_z_score_spectral = scale(spectral_count))
@@ -88,6 +88,64 @@ get_data <- function(what){
 # get the data
 all_data_dicty <- get_data('dicty')
 all_data_neutro <- get_data('neutro')
+
+
+filter_data <- function(celltype, experiment_bool=FALSE, bait_bool=FALSE, condition_bool=FALSE, condition_in=NULL, bait_in=NULL, uniprot_in=NULL, normalized=FALSE){
+  
+  my_data <- NULL
+  my_title <- NULL
+  my_x <- NULL
+  my_y <- if_else(normalized, "z_score_spectral","spectral_count")
+
+  # get summed count for a given uniprot, group per bait_condition (experiment)
+  if (!is.null(uniprot_in) & is.null(condition_in) & experiment_bool){
+    
+    my_x <- 'bait_condition'
+    my_title <- uniprot_in
+    
+    my_data <- filter(get(paste0("all_data_", celltype)), grepl(uniprot_in, uniprot)) %>%
+      group_by(bait, condition) %>%
+      dplyr::summarise(.groups = 'drop', across(my_y, sum)) %>%
+      tidyr::unite(bait_condition, c(bait, condition), sep = "_")
+  }
+  
+  # get count for a given condition
+  else if (!is.null(condition_in) & bait_bool){
+    
+    my_x <- 'bait'
+    my_title <- condition_in
+    
+    my_data <- filter(get(paste0("all_data_", celltype)), grepl(condition_in, condition))
+  }
+  
+  # get count for a given bait
+  else if (!is.null(bait_in) & !experiment_bool){
+
+    my_x <- 'condition'
+    my_title <- bait_in
+    
+    my_data <- filter(get(paste0("all_data_", celltype)), grepl(bait_in, bait))
+    
+    # get count for a given bait group by bait_condition (experiment)
+  }else if (experiment_bool){
+    
+    my_title <- bait_in
+    my_x <- 'bait_condition'
+    
+    my_data <- filter(get(paste0("all_data_", celltype)), grepl(bait_in, bait)) %>%
+      group_by(bait, condition) %>% 
+      tidyr::unite(bait_condition, c(bait, condition), sep = "_")
+  }
+
+  # store labels that can be used to plot
+  my_data_fig_labels <- list()
+  my_data_fig_labels$title <- my_title
+  my_data_fig_labels$x_label <- my_x
+  my_data_fig_labels$y_label <- my_y
+  
+  return(list(count_data = my_data, fig_labels = my_data_fig_labels))
+  
+}
 
 
 #write.table(all_data_dicty , file = "all_data_dicty.txt", row.names = FALSE, quote = FALSE, sep = '\t')
